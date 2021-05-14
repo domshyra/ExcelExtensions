fd6e50fa1413483346b41c22538de49a275613d5
Dom Schira <domshyra@gmail.com>
2021-05-14 15:14:58 -0700

    Added enums for excel

---
+++ ExcelExtensions/Enums/Enums.cs
@@ -0,0 +1,58 @@
+﻿namespace ExcelExtensions.Enums
+{
+    public class Enums
+    {
+        /// <summary>
+        /// Format of data to transcribe to and from excel data into c# objects.
+        /// </summary>
+        public enum ExcelColumnFormatType
+        {
+            /// <summary>
+            /// String type
+            /// </summary>
+            String = 0,
+            /// <summary>
+            /// Decimal type
+            /// </summary>
+            Decimal = 1,
+            /// <summary>
+            /// Dollar type ($)
+            /// </summary>
+            Currency = 2,
+            /// <summary>
+            /// DateTime type
+            /// </summary>
+            Date = 3,
+            /// <summary>
+            /// Time span type
+            /// </summary>
+            Duration = 4,
+            /// <summary>
+            /// Percent type (%)
+            /// </summary>
+            Percent = 5,
+            /// <summary>
+            /// Whole number / int type
+            /// </summary>
+            Int = 6,
+            /// <summary>
+            /// Double type
+            /// </summary>
+            Double = 7,
+            /// <summary>
+            /// Boolean type
+            /// </summary>
+            Bool = 8,
+            /// <summary>
+            /// List of string objects from excel into a dictionary object in c#.
+            /// </summary>
+            /// <remarks>Does not work with exports</remarks>
+            StringList = 9,
+            /// <summary>
+            /// List of decimal objects from excel into a dictionary object in c#.
+            /// </summary>
+            /// <remarks>Does not work with exports</remarks>
+            DecimalList = 10,
+        }
+    }
+}
---
+++ ExcelExtensions/Models/ImportException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO
+namespace ExcelExtensions.Models
+{
+    /// <summary>
+    /// Represents an exception when using the parse funtions of EE
+    /// </summary>
+    public class ImportException : Exception
+    {
+        public ImportException() : base() { }
+        public ImportException(string message) : base(message) { }
+        public ImportException(string message, Exception inner) : base(message, inner) { }
+    }
+}
