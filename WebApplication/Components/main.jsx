9ce40ca42b40efe9ded053293c44cb45d8b01ef2
Dom Schira <domshyra@gmail.com>
2021-05-15 10:23:19 -0700

    Front end packages I think are set up

---
+++ WebApplication/Components/main.jsx
@@ -0,0 +1,10 @@
+﻿import '@popperjs/core';
+import 'bootstrap';
+
+//styles
+import '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/react-fontawesome';
+import '@fortawesome/free-regular-svg-icons';
+import '../Styles/scss/main.scss';
