b6782de7e356ed2db550c1731bc47ffb84c81838
Dom Schira <domshyra@gmail.com>
2021-05-14 18:01:24 -0700

    Added summaries

--- ExcelExtensions/Providers/IFileAndSheetValidatior.cs
+++ ExcelExtensions/Providers/IFileAndSheetValidatior.cs
@@ -4,24 +4,25 @@ using OfficeOpenXml;
 namespace ExcelExtensions.Interfaces
 {
     /// <summary>
-    /// 
+    /// Provides methods to get <see cref="ExcelWorksheet"/> and <see cref="ExcelPackage"/>.
     /// </summary>
     public interface IFileAndSheetValidatior
     {
         /// <summary>
-        /// 
+        /// Provides an <see cref="ExcelPackage"/> from the excel file.
         /// </summary>
         /// <param name="excelFile"></param>
         /// <param name="password"></param>
         /// <returns></returns>
+        /// <exception cref="ImportException">thrown if the file is null or not an excel file.</exception>
         ExcelPackage GetExcelPackage(dynamic excelFile, string password = "");
         /// <summary>
-        /// 
+        /// Provides an <see cref="ExcelWorksheet"/> by sheet name. 
         /// </summary>
         /// <param name="package"></param>
         /// <param name="sheetName"></param>
         /// <param name="exception">Returns a <see cref="ParseException"/></param>
-        /// <returns></returns>
+        /// <returns>Null if No file found with an <see cref="ParseException"/>, or with a <see cref="ExcelWorksheet"/> and null <paramref name="exception"/></returns>
         ExcelWorksheet GetExcelWorksheet(ExcelPackage package, string sheetName, out ParseException exception);
     }
 }
