bb62047b7c26483d083db01a4c68bb30bc5dc909
Dom Schira <domshyra@gmail.com>
2021-05-18 17:33:28 -0700

    working on react componets

--- WebApplication/Components/UploadForm.jsx
+++ WebApplication/Components/UploadForm.jsx
@@ -1,47 +1,33 @@
 ﻿import React from 'react';
+import bsCustomFileInput from 'bs-custom-file-input';
+
 
 function UploadForm() {
+
+    useEffect(() => {
+        bsCustomFileInput.init()
+    }, []);
+
     return (
         <>
             <h2 className="text-primary">{props.FormTitle}</h2>
             <div className="row mb-2">
                 <div className="col-12">
                     <div className="bg-light px-3 pt-2 pb-3 border rounded">
-                        <form asp-action="SearchAndImportTable" asp-controller="ExcelExtensions" enctype="multipart/form-data" method="post" className="form-horizontal needs-validation" novalidate>
+                        <form asp-action="ImportTable" asp-controller="Home" enctype="multipart/form-data" method="post" className="form-horizontal needs-validation" novalidate>
                             <div className="form-group">
-                                <label>Table line items</label>
+                                <label>Table file</label>
                                 <div className="custom-file">
-                                    <input type="file" className="custom-file-input" name="file" accept=".xls,.xlsx" required>
-                                        <label className="custom-file-label" for="customFile">Choose file</label>
-                                        </div>
-                                    <small className="form-text text-muted">
-                                        This requires an excel sheet a sheet named "Sheet1".
-                                        </small>
+                                    <input type="file" className="custom-file-input" name="file" accept=".xls,.xlsx" required />
+                                    <label className="custom-file-label" for="customFile">Choose file</label>
                                 </div>
-
-                                <button className="btn btn-primary" type="submit">Import</button>
-                            </div>
-
-                        </form>
-                        {/*TODO:Make this output into a react componet*/}
-                        <div className="playground-output-content">
-                            <h5 className="mb-0">
-                                JSON output
-                                <a id="excel-search-table-json-output-toggler" className="font-weight-bold pull-right" data-toggle="collapse" href="#search-table-json-output" aria-expanded="true" aria-controls="json-output">
-                                    <i className="fa fa-chevron-down cb-header-chevron"></i>
-                                </a>
-                            </h5>
-                            <div className="row collapse show mb-2 pt-2" id="search-table-json-output">
-                                <div className="col-12">
-                                    <div className="playground-json-output" id="search-table-json">
-                                        <pre className="playground-code-wrap" id="search-table-json-txt"></pre>
-                                    </div>
                                     <small className="form-text text-muted">
-                                        Json format is "Row #": {rowModel}
+                                        This requires an excel sheet a sheet named { props.SheetName }.
                                     </small>
                                 </div>
-                            </div>
-                        </div>
+                                <button className="btn btn-primary" type="submit">Import</button>
+                        </form>
+                        {/*TODO: add back react component*/}
                     </div>
                 </div>
             </div>
