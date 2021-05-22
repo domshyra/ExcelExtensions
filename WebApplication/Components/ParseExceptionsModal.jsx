8b6a5c0bd2a93ad87e7579febf1f6b575b3404b1
Dom Schira <domshyra@gmail.com>
2021-05-22 12:24:23 -0700

    Mabye set the modal up to show when we have exceptions, but no data is being passed in

--- WebApplication/Components/ParseExceptionsModal.jsx
+++ WebApplication/Components/ParseExceptionsModal.jsx
@@ -1,8 +1,19 @@
-﻿import React from 'react';
+﻿import { Modal } from 'bootstrap';
+import React, { useState, useEffect, useRef } from 'react';
 import ParseExceptions from './ParseExceptions';
 import PropTypes from 'prop-types';
 
 const ParseExceptionsModal = (props) => {
+    const [modal, setModal] = useState(null);
+    const parseExceptionModal = useRef();
+
+    useEffect(() => {
+        setModal(new Modal(parseExceptionModal.current));
+        if (props.json.length > 0) {
+            modal.show();
+        }
+    }, []);
+
     return (
         <>
             <div
@@ -10,24 +21,22 @@ const ParseExceptionsModal = (props) => {
                 className="modal fade"
                 tabIndex="-1"
                 role="dialog"
+                ref={parseExceptionModal}
                 aria-labelledby="parse-exceptions-modal"
                 aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title">{ props.title}</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+                            <h5 className="modal-title">{props.title}</h5>
+                            <button type="button" className="btn-close" onClick={() => modal.hide()} aria-label="Close">
+                                {/*<span aria-hidden="true">&times;</span>*/}
                             </button>
                         </div>
                         <div className="modal-body">
                             <ParseExceptions json={props.json} />
                         </div>
                         <div className="modal-footer">
-                            <button
-                                data-dismiss="modal"
-                                type="button"
-                                className="btn btn-outline-secondary">
+                            <button onClick={() => modal.hide()} type="button" className="btn btn-outline-secondary">
                                 Dismiss
                             </button>
                         </div>
--- WebApplication/wwwroot/public/main.bundle.js
+++ WebApplication/wwwroot/public/main.bundle.js
@@ -6093,54 +6093,84 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var _ParseExceptions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ParseExceptions */ "./Components/ParseExceptions.jsx");
-/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
-/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var _ParseExceptions__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ParseExceptions */ "./Components/ParseExceptions.jsx");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_3__);
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr && (typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]); if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+
 
 
 
 
 var ParseExceptionsModal = function ParseExceptionsModal(props) {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(null),
+      _useState2 = _slicedToArray(_useState, 2),
+      modal = _useState2[0],
+      setModal = _useState2[1];
+
+  var parseExceptionModal = (0,react__WEBPACK_IMPORTED_MODULE_1__.useRef)();
+  (0,react__WEBPACK_IMPORTED_MODULE_1__.useEffect)(function () {
+    setModal(new bootstrap__WEBPACK_IMPORTED_MODULE_0__.Modal(parseExceptionModal.current));
+
+    if (props.json.length > 0) {
+      modal.show();
+    }
+  }, []);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement(react__WEBPACK_IMPORTED_MODULE_1__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", {
     id: "parse-exceptions-modal",
     className: "modal fade",
     tabIndex: "-1",
     role: "dialog",
+    ref: parseExceptionModal,
     "aria-labelledby": "parse-exceptions-modal",
     "aria-hidden": "true"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", {
     className: "modal-dialog",
     role: "document"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", {
     className: "modal-content"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", {
     className: "modal-header"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h5", {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("h5", {
     className: "modal-title"
-  }, props.title), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+  }, props.title), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("button", {
     type: "button",
-    className: "close",
-    "data-dismiss": "modal",
+    className: "btn-close",
+    onClick: function onClick() {
+      return modal.hide();
+    },
     "aria-label": "Close"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
-    "aria-hidden": "true"
-  }, "\xD7"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", {
     className: "modal-body"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_ParseExceptions__WEBPACK_IMPORTED_MODULE_1__.default, {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement(_ParseExceptions__WEBPACK_IMPORTED_MODULE_2__.default, {
     json: props.json
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("div", {
     className: "modal-footer"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
-    "data-dismiss": "modal",
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("button", {
+    onClick: function onClick() {
+      return modal.hide();
+    },
     type: "button",
     className: "btn btn-outline-secondary"
   }, "Dismiss"))))));
 };
 
 ParseExceptionsModal.propTypes = {
-  json: (prop_types__WEBPACK_IMPORTED_MODULE_2___default().array),
-  title: (prop_types__WEBPACK_IMPORTED_MODULE_2___default().string)
+  json: (prop_types__WEBPACK_IMPORTED_MODULE_3___default().array),
+  title: (prop_types__WEBPACK_IMPORTED_MODULE_3___default().string)
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ParseExceptionsModal);
 
